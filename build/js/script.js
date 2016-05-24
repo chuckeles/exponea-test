315f1573e954e7ccdbfa8ef31cc0c811415169de
Martin Chuckeles <martin@jakubik.info>
2016-05-24 17:16:53 +0200

    Make breadcrumbs use the link and scope

--- build/js/script.js
+++ build/js/script.js
@@ -31903,7 +31903,15 @@ angular.module("exponeaApp")
   .directive("breadcrumbs", function breadcrumbs() {
     return {
       restrict: "E",
-      templateUrl: "directives/breadcrumbs.html"
+      templateUrl: "directives/breadcrumbs.html",
+      scope: {},
+      link: function breadcrumbsLink(scope) {
+        // This could be retrieved from the router.
+        scope.path = [
+          "Dashboards"
+        ];
+        scope.active = "User Activity";
+      }
     };
   });
 
--- js/breadcrumbs.js
+++ js/breadcrumbs.js
@@ -3,6 +3,14 @@ angular.module("exponeaApp")
   .directive("breadcrumbs", function breadcrumbs() {
     return {
       restrict: "E",
-      templateUrl: "directives/breadcrumbs.html"
+      templateUrl: "directives/breadcrumbs.html",
+      scope: {},
+      link: function breadcrumbsLink(scope) {
+        // This could be retrieved from the router.
+        scope.path = [
+          "Dashboards"
+        ];
+        scope.active = "User Activity";
+      }
     };
   });
