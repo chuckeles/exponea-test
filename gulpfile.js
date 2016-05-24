a9f69ebe4a24b2558fbda10879d05bab54b6defb
Martin Chuckeles <martin@jakubik.info>
2016-05-24 11:37:50 +0200

    Add CSS and JS gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,34 @@ var browserSync = require("browser-sync").create();
 var gulp        = require("gulp");
 
 /**
+ * Gulp plugins.
+ */
+var concat  = require("gulp-concat");
+var plumber = require("gulp-plumber");
+
+/**
  * Input files. HTML, CSS, JS.
  */
 var input = {
   html: [
     "index.html"
   ],
-  scss: [
-    "bower_components/normalize-css/normalize.css"
+  css: [
+    "bower_components/normalize-css/normalize.css",
+    "css/**/*.css"
+  ],
+  js: [
+    "bower_components/angular/angular.js",
+    "js/**/*.js"
   ]
+};
+
+/**
+ * Where to put the output.
+ */
+var output = {
+  css: "build/css",
+  js: "build/js"
 };
 
 /**
@@ -26,6 +45,8 @@ gulp.task("default", ["watch", "serve"]);
  */
 gulp.task("watch", function watch() {
   gulp.watch(input.html, ["html"]);
+  gulp.watch(input.css,  ["css"]);
+  gulp.watch(input.js, ["js-reload"]);
 });
 
 /**
@@ -46,4 +67,30 @@ gulp.task("serve", function serve() {
  */
 gulp.task("html", function html() {
   browserSync.reload();
+});
+
+/**
+ * Concatenate CSS. Also inject into the browser.
+ */
+gulp.task("css", function css() {
+  return gulp.src(input.css)
+    .pipe(plumber())
+    .pipe(concat("style.css"))
+    .pipe(gulp.dest(output.css))
+    .pipe(browserSync.stream());
+});
+
+/**
+ * Run the JS task and reload the browser.
+ */
+gulp.task("js-reload", ["js"], browserSync.reload);
+
+/**
+ * Concatenate JS.
+ */
+gulp.task("js", function js() {
+  return gulp.src(input.js)
+    .pipe(plumber())
+    .pipe(concat("script.js"))
+    .pipe(gulp.dest(output.js));
 });
